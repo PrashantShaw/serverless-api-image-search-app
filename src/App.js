b91313fa109032c2428c7ea39d931fdbb5337c43
Prashant Shaw <prashant.shaw.3154@gmail.com>
2023-01-20 15:56:42 +0530

    created serverless unspalsh image search app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { DebounceInput } from 'react-debounce-input'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [searchText, setSearchText] = useState('')
+  const [images, setImages] = useState([])
+  const serverless_API_URL = 'https://serverless-api.prashantshaw.workers.dev'
+  // const serverless_API_URL = 'http://127.0.0.1:8787'
+
+  function getImages(text) {
+    return fetch(
+      serverless_API_URL,
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({ searchText: text })
+      }
+    )
+  }
+
+  useEffect(() => {
+    if (!searchText) return
+
+    getImages(searchText)
+      .then(res => res.json())
+      .then(data => setImages(data))
+  }, [searchText])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' >
+      <div className='search-box'>
+        <DebounceInput
+          className='search'
+          // minLength={2}
+          value={searchText}
+          placeholder="Search image .."
+          debounceTimeout={500}
+          onChange={(e) => { setSearchText(e.target.value) }}
+        />
+      </div>
+      <div className='images'>
+        {images.map((imageObj, idx) => (
+          <a href={imageObj.link}>
+            <img src={imageObj.url} alt='something' />
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
